a471bd5b036f3c4b83e62129f8ba14a4dc5b4ef6
jeongkyueun <kyueun@swu.ac.kr>
2025-05-13 23:56:21 +0900

    [9주차] 정규은 과제 제출

---
+++ Week09/netflix/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div style={{
+      height: '100vh',
+      backgroundColor: '#000000',
+      color: '#fff',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <h1>넷플릭스에 오신 걸 환영합니다!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
