7857301a055c00723d03f9465e8ad75f019f3cce
SongJi-eun6 <ssjieun32@gmail.com>
2025-05-26 22:20:22 +0900

    [11주차] 송지은 과제 제출

---
+++ Week11_2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import axios from 'axios';
+import MovieCard from './components/MovieCard';
+import MovieFavorites from './components/MovieFavorites';
+
+function App() {
+  return(
+    <div className='App'>
+      <MovieFavorites/>
+    </div>
+  );
+}
+
+export default App;
